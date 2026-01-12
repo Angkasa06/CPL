393c58a87dec053aca1ab817a31a870f1532ae77
LERO <leonardoangkasa2006@gmail.com>
2026-01-12 12:03:22 +0700

    Update Update

--- Simple Sorting n Searching.cpp
+++ Simple Sorting n Searching.cpp
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
-void SelectionSort(int n, int arr[]) {
+void Sorting(int n, int arr[]) {
 	int Temp, minIndex;
-	
 	for(int i = 0; i < n-1; i++) {
 		minIndex = i;
 		for(int j = i+1; j < n; j++) {
@@ -22,45 +21,45 @@ void SelectionSort(int n, int arr[]) {
 	}
 }
 
-int BinarySearch(int x, int arr[], int Min, int Max) {
+int Searching(int x, int arr[], int Min, int Max) {
 	if(Min > Max) {
-		return 0;
+		return -1;
 	}
 	
 	int Mid = (Min + Max) / 2;
 	
 	if(arr[Mid] == x) {
-		return 1;
+		return Mid;
 	} else if(arr[Mid] > x) {
-		return BinarySearch(x, arr, Min, Mid-1);
-	} else return BinarySearch(x, arr, Mid+1, Max);
-	
-	return 0;
+		return Searching(x, arr, Min, Mid-1);
+	} else {
+		return Searching(x, arr, Mid+1, Max);
+	}
 }
 
 int main() {
-	int x;	
-	printf("Angka yang dicari? ");
+	int x;
+	printf("> ");
 	scanf("%d", &x);
 	
 	int num;
-	printf("Banyak data?       ");
+	printf("> ");
 	scanf("%d", &num);
 	
-	// Input Data
 	int ar[num];
 	printf("> ");
 	for(int i = 0; i < num; i++) {
 		scanf("%d", &ar[i]);
 	}
 	
-	// Syarat Binary Sort: Data harus diurut dari terkecil ke terbesar
-	SelectionSort(num, ar);
+	// Selection Sort
+	Sorting(num, ar);
 	
-	// Mencari nilai yang kita inginkan
-	if(BinarySearch(x, ar, 0, num-1)) {
-		printf("\nAda");
-	} else printf("\nTidak ada");
+	// Binary Search
+	int idx = Searching(x, ar, 0, num-1);
+	if(idx != -1) {
+		printf("\nThe number you are looking for is at index %d", idx);
+	} else printf("\nNumber not found.");
 	
 	return 0;
 }
